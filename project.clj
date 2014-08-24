a372677e35061800101d1b48a32fd46ebbe80757
Greg Sexton <gregsexton@gmail.com>
2014-08-24 23:17:16 +0100

    Add support for extracting definitions

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [org.clojure/tools.analyzer.jvm "0.5.4"]
                  [clojurewerkz/elastisch "2.1.0-beta4"]]
   :main pathfinder.core
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]
+                                  [org.clojure/tools.namespace "0.2.5"]]
                    :plugins [[lein-midje "3.1.3"]]}
              :uberjar {:aot :all}})
--- src/pathfinder/analyze/clojure.clj
+++ src/pathfinder/analyze/clojure.clj
@@ -16,7 +16,14 @@
     (take-while #(not (identical? % eof))
                 (repeatedly #(reader/read pbr false eof)))))
 
-(defn- extract-data-model [ast])
+(defn- extract-data-model [ast meta])
+
+;; TODO: using jvm-analyzer we can do a full parse and build a
+;; complete, perfectly accurate, ast. This is essentially the front half
+;; of the clojure compiler. It would imply loading code in at the
+;; granularity of a project. We would probably need to understand the
+;; build system too. For now I've hacked up a fairly basic parser based
+;; on heuristics, see CljHeuristicAnalyzer below.
 
 (deftype CljAnalyzer []
   Analyzer
@@ -24,11 +31,53 @@
     (-> source
         read-forms
         (->> (map jvm-analyzer/analyze))
-        #_extract-data-model)))
+        #_(extract-data-model meta))))
+
+(defn- extract-ns [state form]
+  (if (and (list? form) (= (first form) 'ns))
+    (assoc state :ns (second form))
+    state))
+
+(defn- extract-definition [state form]
+  ;; support only the clojure.core types for now
+  (let [types '{def :definition
+                definline :function
+                definterface :interface
+                defmacro :macro
+                defmethod :method
+                defmulti :multimethod
+                defn :function
+                defn- :function
+                defprotocol :protocol
+                defrecord :record
+                defstruct :struct
+                deftype :class}]
+    (if (and (list? form) (.startsWith (str (first form)) "def")) ;crude
+      (update-in state [:model :definitions]
+                 conj {:name (str (:ns state) (if (:ns state) "/" "") (second form))
+                       :type (get types (first form) :unknown)
+                       :pos (select-keys (meta form) [:column :line :end-column :end-line])})
+      state)))
+
+;;; TODO:
+(defn- extract-usages [state form] state)
+
+(defn- guess-data-model [forms]
+  (->> forms
+       (reduce (fn [state form]
+                 (-> state
+                     (extract-ns form)
+                     (extract-definition form)
+                     (extract-usages form)))
+               {:ns nil :model {}})
+       :model))
 
 (deftype CljHeuristicAnalyzer []
   ;; guess without full parse and eval context
   Analyzer
   (analyze [this source meta]
     (-> source
-        read-forms)))
+        read-forms
+        guess-data-model
+        (merge {:source source
+                :meta meta}))))
