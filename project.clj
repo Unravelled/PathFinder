b90a3a029063b54ef8b5354df31b243f97b47df8
Greg Sexton <gregsexton@gmail.com>
2014-09-04 21:37:12 +0100

    Sort dependencies

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   :url "https://github.com/IHopeYouCanChangeThis/PathFinder"
   :license {:name "Apache License"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.8.7"]
-                 [org.clojure/tools.analyzer.jvm "0.5.4"]
-                 [clojurewerkz/elastisch "2.1.0-beta4"]
+  :dependencies [[clojurewerkz/elastisch "2.1.0-beta4"]
                  [compojure "1.1.8"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.analyzer.jvm "0.5.4"]
+                 [org.clojure/tools.reader "0.8.7"]
                  [ring/ring-jetty-adapter "1.3.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [javax.servlet/servlet-api "2.5"]
-                                  [ring-mock "0.1.5"]
-                                  [org.clojure/tools.namespace "0.2.5"]]
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [midje "1.6.3"]
+                                  [org.clojure/tools.namespace "0.2.5"]
+                                  [ring-mock "0.1.5"]]
                    :plugins [[lein-midje "3.1.3"]]
                    :source-paths ["dev"]}
              :uberjar {:aot :all}})
