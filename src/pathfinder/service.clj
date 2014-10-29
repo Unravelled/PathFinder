7b186ca59b01e88277e4ba4a302553d7b70e429d
trogers84@gmail.com <trogers84@gmail.com>
2014-10-29 08:03:03 +0000

    rearranged routes, removing liberator, for now

--- src/pathfinder/service.clj
+++ src/pathfinder/service.clj
@@ -1,25 +1,55 @@
 (ns pathfinder.service
   (:require [liberator.core :refer [resource defresource]]
             [compojure.core :refer :all]
+            [ring.middleware.json as middleware]
             [ring.middleware.params :refer [wrap-params]]
             [compojure.route :as route]))
 
-(defresource project []
-  :available-media-types ["application/json"]
-  :handle-ok (fn [_] "Hello, Project"))
+(defn get-all-projects []
+  "Sure")
 
-(defresource file []
-  :available-media-types ["application/json"]
-  :handle-ok (fn [_] "Hello, File"))
+(defn index-project [body]
+  "Sounds good")
 
-(defresource query []
-  :available-media-types ["application/json"]
-  :handle-ok (fn [_] "Hello, Query"))
+(defn get-project [id]
+  "No")
+
+(defn update-project [id body]
+  "Bad idea")
+
+(defn delete-project [id]
+  "Really bad idea")
+
+(defn get-file [path]
+  "file")
+
+(defn store-file [path body]
+  "I am full")
+
+(defn update-file [path body]
+  "Are you sure?")
+
+(defn delete-file [path]
+  "Shouldnt")
 
 (defroutes main-routes
-  (GET "/project" [] (project))
-  (GET "/file" [] (file))
-  (GET "/query" [] (query))
-  (route/resources "/")
+  (context "/projects" [] (defroutes project-routes
+                            (GET "/" [] (get-all-projects))
+                            (POST "/" {body :body} (index-project body))
+                            (context "/:id" [id] (defroutes project-routes
+                                                   (GET "/" [] (get-project id))
+                                                   (PUT "/" {body :body} (update-project id body))
+                                                   (DELETE "/" [] (delete-project id))))))
+  (context "/file/:path" [path] (defoutes file-routes
+                              (GET "/" [] (get-file path))
+                              (POST "/" {body :body} (store-file path body))
+                              (PUT "/" {body :body} (update-file path body))
+                              (DELETE "/" [] (delete-file path))))
+
   (route/not-found "Not Found"))
 
+
+(def app
+  (-> (handler/api main-routes)
+      (middleware/wrap-json-body)
+      (middleware/wrap-json-response))
